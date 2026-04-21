3900656060a70040d726f3e162304fe638074a1f
Haider16371 <119101580+Haider16371@users.noreply.github.com>
2026-04-21 09:23:15 +0300

    Update main.c

--- installer/source/main.c
+++ installer/source/main.c
@@ -196,7 +196,7 @@ int _main(struct thread *td) {
 
   // Create temp file to prevent re-running HEN
   touch_file(IS_INSTALLED_PATH);
-  printf_notification("Welcome to HEN %s", VERSION);
+  printf_notification("HEN edit By Haider Version 1.0%s", VERSION);
 
   const char *proc = kill_ui ? "SceShellUI" : NULL;
   if (kill_ui) {
